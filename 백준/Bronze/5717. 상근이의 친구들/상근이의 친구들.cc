439d45501700d047bbf92173b9201810ce25197a
성윤 seongyun <102134353+EnoughKK@users.noreply.github.com>
2025-07-05 19:16:21 +0900

    [Bronze V] Title: 상근이의 친구들, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5717. 상근이의 친구들/상근이의 친구들.cc
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int m, f;
+
+int main()
+{
+    ios_base::sync_with_stdio(false); cout.tie(NULL); cin.tie(NULL);
+
+    while(1)
+    {
+        cin >> m >> f;
+
+        if(m == 0 && f == 0) break;
+
+        cout << m + f << "\n";
+    }
+    
+
+    return 0;
+}
