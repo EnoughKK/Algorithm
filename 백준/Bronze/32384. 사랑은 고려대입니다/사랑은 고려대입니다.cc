49f6a313cdd4cbeb079bb713ce6c75a3c91e9069
성윤 seongyun <102134353+EnoughKK@users.noreply.github.com>
2025-07-07 02:49:52 +0900

    [Bronze V] Title: 사랑은 고려대입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32384. 사랑은 고려대입니다/사랑은 고려대입니다.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n;
+
+int main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+    cin >> n;
+
+    for(int i = 0; i < n; i++)
+    {
+        cout << "LoveisKoreaUniversity" << " ";
+    }
+
+    return 0;
+}
