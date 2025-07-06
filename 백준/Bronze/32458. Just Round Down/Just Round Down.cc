803155dcd325519d2701523898e57e96acb2b20a
성윤 seongyun <102134353+EnoughKK@users.noreply.github.com>
2025-07-07 02:54:32 +0900

    [Bronze V] Title: Just Round Down, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32458. Just Round Down/Just Round Down.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+float x;
+
+int main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+    cin >> x;
+
+    cout << (int) x;
+
+    return 0;
+}
