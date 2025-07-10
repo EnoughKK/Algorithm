37880ef28c5a0c9ab2ac84c4b622dc9034d8d0a6
성윤 seongyun <102134353+EnoughKK@users.noreply.github.com>
2025-07-11 04:18:32 +0900

    [Bronze V] Title: 홍익대학교, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/16394. 홍익대학교/홍익대학교.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    cin >> n;
+
+    cout << n - 1946;
+
+    return 0;
+}
