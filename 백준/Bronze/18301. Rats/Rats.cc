2ef3892d42a53137652f5ca7ce2703113deefa81
성윤 seongyun <102134353+EnoughKK@users.noreply.github.com>
2025-07-26 01:45:11 +0900

    [Bronze V] Title: Rats, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/18301. Rats/Rats.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n1, n2, n12;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    cin >> n1 >> n2 >> n12;
+
+    cout << (n1 + 1) * (n2 + 1) / (n12 + 1) - 1;
+
+    return 0;
+}
