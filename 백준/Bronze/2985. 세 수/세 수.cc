58947ddfd5dcc7a6fa8644bb9cdb96f17f80da89
성윤 seongyun <102134353+EnoughKK@users.noreply.github.com>
2025-08-13 05:41:00 +0900

    [Bronze III] Title: 세 수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2985. 세 수/세 수.cc
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int a, b, c;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    cin >> a >> b >> c;
+
+    if(a + b == c) cout << a << '+' << b << '=' << c << '\n';
+    else if(a - b == c) cout << a << '-' << b << '=' << c << '\n';
+    else if(a * b == c) cout << a << '*' << b << '=' << c << '\n';
+    else if(a / b == c) cout << a << '/' << b << '=' << c << '\n';
+    else if(a == b + c) cout << a << '=' << b << '+' << c << '\n';
+    else if(a == b - c) cout << a << '=' << b << '-' << c << '\n';
+    else if(a == b * c) cout << a << '=' << b << '*' << c << '\n';
+    else if(a == b / c) cout << a << '=' << b << '/' << c << '\n';
+
+    return 0;
+}
